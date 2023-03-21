be9b4e4fc7dfd252c562011bbe55b0de7f33f25b
Rabm1504 <rennyangel2@gmail.com>
2023-03-21 12:03:52 -0500

    6

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
+import { BrowserRouter } from 'react-router-dom';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -14,7 +14,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(200 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate=["Web Developer", "Web Designer", "Full-Stack"];
+  const toRotate = ["Web Developer", "Web Designer", "Full-Stack"];
   const period = 500;
 
   useEffect(() => {
@@ -52,32 +52,34 @@ export const Banner = () => {
 
   return (
     <section className="banner" id="home">
-      <Container>
-        <Row className="aligh-items-center">
-          <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                  <h1>{`Hi! I'm Renny Belandria`} <br></br> </h1>
-                  <h4><span className="txt-rotate" dataperiod="1000" data-rotate='[ "Web Developer", "Web Designer", "Full-Stack" ]'><span className="wrap">{text}</span></span></h4>
-                  <p>I am a cheerful, responsible, and proactive person. I'm studying in the Full-Stack Development Bootcamp at Prográmate-Academy powered by EducaMás, GOYN & Simplon partnership program. Through these experiences, I have learned Web Development with the MERN Stack.</p>
-                  <Router>
-                    <HashLink to="#bio" style={{textDecoration:"none"}}>
-                      <button onClick={() => console.log('connect')}>Let’s go ahead! <ArrowRightCircle size={25} /></button>
-                    </HashLink>
-                  </Router>
-              </div>}
-            </TrackVisibility>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              { <div className="svg">
-                  <iframe src="https://cdn.svgator.com/assets/main-page/fold1/astronaut-hero.svg" alt="Header Img"/>
+      <BrowserRouter>
+        <Container>
+          <Row className="aligh-items-center">
+            <Col xs={12} md={6} xl={7}>
+              <TrackVisibility>
+                {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                    <h1>{`Hi! I'm Renny Belandria`} <br></br> </h1>
+                    <h4><span className="txt-rotate" dataperiod="1000" data-rotate='[ "Web Developer", "Web Designer", "Full-Stack" ]'><span className="wrap">{text}</span></span></h4>
+                    <p>I am a cheerful, responsible, and proactive person. I'm studying in the Full-Stack Development Bootcamp at Prográmate-Academy powered by EducaMás, GOYN & Simplon partnership program. Through these experiences, I have learned Web Development with the MERN Stack.</p>
+                    <Router>
+                      <HashLink to="#bio" style={{ textDecoration: "none" }}>
+                        <button onClick={() => console.log('connect')}>Let’s go ahead! <ArrowRightCircle size={25} /></button>
+                      </HashLink>
+                    </Router>
+                  </div>}
+              </TrackVisibility>
+            </Col>
+            <Col xs={12} md={6} xl={5}>
+              <TrackVisibility>
+                {<div className="svg">
+                  <iframe src="https://cdn.svgator.com/assets/main-page/fold1/astronaut-hero.svg" alt="Header Img" />
                 </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
+              </TrackVisibility>
+            </Col>
+          </Row>
+        </Container>
+      </BrowserRouter>
     </section>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter } from 'react-router-dom';
 import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -17,10 +18,10 @@ export const Contact = () => {
   const [status, setStatus] = useState({});
 
   const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
+    setFormDetails({
+      ...formDetails,
+      [category]: value
+    })
   }
 
   const handleSubmit = async (e) => {
@@ -51,9 +52,9 @@ export const Contact = () => {
   Thank you for your time!
   `
 
-  const url=`https://api.whatsapp.com/send?phone=573244620334&text=${encodeURIComponent(mensaje)}`;
+  const url = `https://api.whatsapp.com/send?phone=573244620334&text=${encodeURIComponent(mensaje)}`;
 
-  const handleClick=(e) => {
+  const handleClick = (e) => {
     e.preventDefault();
     window.open(url);
     setFormDetails(formInitialDetails);
@@ -61,51 +62,53 @@ export const Contact = () => {
 
   return (
     <section className="contact" id="connect">
-      <Container>
-        <Row className="align-items-center">
-          <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
-              }
-            </TrackVisibility>
-          </Col>
-          <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Let's Connect</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                        <button onClick={handleClick}><span>Send through WhatsApp</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                      </Col>
-                    }
-                  </Row>
-                </form>
-              </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
+      <BrowserRouter>
+        <Container>
+          <Row className="align-items-center">
+            <Col size={12} md={6}>
+              <TrackVisibility>
+                {({ isVisible }) =>
+                  <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
+                }
+              </TrackVisibility>
+            </Col>
+            <Col size={12} md={6}>
+              <TrackVisibility>
+                {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                    <h2>Let's Connect</h2>
+                    <form onSubmit={handleSubmit}>
+                      <Row>
+                        <Col size={12} sm={6} className="px-1">
+                          <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                        </Col>
+                        <Col size={12} sm={6} className="px-1">
+                          <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                        </Col>
+                        <Col size={12} sm={6} className="px-1">
+                          <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                        </Col>
+                        <Col size={12} sm={6} className="px-1">
+                          <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                        </Col>
+                        <Col size={12} className="px-1">
+                          <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                          <button onClick={handleClick}><span>Send through WhatsApp</span></button>
+                        </Col>
+                        {
+                          status.message &&
+                          <Col>
+                            <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                          </Col>
+                        }
+                      </Row>
+                    </form>
+                  </div>}
+              </TrackVisibility>
+            </Col>
+          </Row>
+        </Container>
+      </BrowserRouter>
     </section>
   )
 }
